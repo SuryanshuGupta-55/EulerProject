f9a5ecafef324df56201b3794ed37c92cac70a8f
Suryanshu Gupta <64960056+SuryanshuGupta-55@users.noreply.github.com>
2020-08-20 12:39:03 +0530

    Add files via upload

---
+++ src/Problem10.java
@@ -0,0 +1,178 @@
+import java.io.*;
+import java.util.Arrays;
+
+class Problem10 {
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length 
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    static Reader sc = new Reader();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static boolean isPrime(int num){
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i == 0)
+                return false;
+        }
+        return true;
+    }
+   /* public static long sievesum(int num){
+        long sum = 0;
+        boolean[] primes = new boolean[num];
+        Arrays.fill(primes,true);
+        primes[0] = false;
+        primes[1] = false;
+        for(int i=2;i<num;i++){
+            if(primes[i] == true){
+                for(int j=i*i;j<num;j+=i){
+                    primes[j] = false;
+                }
+            }
+        }
+        for(int i=2;i<primes.length;i++){
+            if(primes[i] == true)
+                sum +=i;
+        }
+        return sum;
+    }*/
+
+
+    public static void main(String args[]) throws IOException {
+        int n = 2000000;
+        long sum = 0;
+        for(int i=2;i<=n;i++) {
+            if (isPrime(i))
+                sum += i;
+        }
+        println(sum+" ");
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static int inputInt() throws IOException {
+        return sc.nextInt();
+    }
+
+    public static long inputLong() throws IOException {
+        return sc.nextLong();
+    }
+
+    public static double inputDouble() throws IOException {
+        return sc.nextDouble();
+    }
+
+    public static String inputString() throws IOException {
+        return sc.readLine();
+    }
+
+    public static void print(String a) throws IOException {
+        bw.write(a);
+    }
+
+    public static void printSp(String a) throws IOException {
+        bw.write(a + " ");
+    }
+
+    public static void println(String a) throws IOException {
+        bw.write(a + "\n");
+    }
+}
